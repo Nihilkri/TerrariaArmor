bf71d773ada5b634d687139aeb945c34d264353f
NihilKri <nehilkori@gmail.com>
2015-08-09 00:30:19 -0500

    Kin'silir

---
+++ TerrariaArmor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaArmor {
+	class Program {
+		#region Variables
+			struct armor {
+				string name;
+				string slot;
+				int def;
+				int dmg;
+				int crit;
+				int cost;
+				int mp;
+			};
+			armor[] armors = new armor[10];
+			struct spell {
+				string name;
+				int dmg;
+				int mp;
+				int crit;
+				int ut;
+			}
+		#endregion Variables
+		static void Main(string[] args) {
+
+
+		}
+	}
+}
